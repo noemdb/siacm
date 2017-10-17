3654dc5e7704a082124dc8a0a32415f20772de8c
noemdb <noemdb@gmail.com>
2017-10-16 23:31:47 -0400

    Integración SBAdmin

--- routes/web_sbadmin.php
+++ routes/web_sbadmin.php
@@ -11,20 +11,6 @@
 |
 */
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
 Route::get('/', function () {
     return view('admin.home');
 });
